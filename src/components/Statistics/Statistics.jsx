ef283076892ee1f920f27cefebfafdf862fb42b2
AnttonnIT <anttonnit@gmail.com>
2023-02-02 20:19:06 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -19,7 +19,7 @@ const createColor = () => {
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      {title.length > 0 && <h2 className={css.title}>Upload stats</h2>}
+      {title && <h2 className={css.title}>Upload stats</h2>}
 
       <ul className={css.statList}>
         {stats.map(({ id, label, percentage }) => {
